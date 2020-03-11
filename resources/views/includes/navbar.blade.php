432a03ecce5dc2fcb04e0cd37a54a0d7297f0d0a
Dilovar Azimov <dilovar09@gmail.com>
2020-03-11 14:04:35 +0300

    recovered backtotop button

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,6 +1,6 @@
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-@if (!Auth::guest())<button  id="menu-toggle"> <span class="navbar-toggler-icon"></span></button>@endif
+@if (!Auth::guest())<div  id="menu-toggle"> <span class="navbar-toggler-icon"></span></div>@endif
 <a class="navbar-brand ml-2" href="/">Pilot Auto</a>  
 </nav>
 
